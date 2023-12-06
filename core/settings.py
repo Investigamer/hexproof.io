73a3f43d4877ecb3e2f1e899dbd499ce9f91d8a2
Investigamer <freethoughtleft@gmail.com>
2023-12-06 10:53:56 -0600

    fix(django): Add support for staticroot
    Signed-off-by: Investigamer <freethoughtleft@gmail.com>

--- core/settings.py
+++ core/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticroot'
+STATIC_ROOT.mkdir(mode=755, parents=True, exist_ok=True)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
